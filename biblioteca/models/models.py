7a083566d02172f0c5ec6c34030e9cd452508073
apavon <bc.apavon.1995@iespabloserrano.com>
2023-01-19 11:30:09 +0000

    Biblioteca y Garaje

--- biblioteca/models/models.py
+++ biblioteca/models/models.py
@@ -1,11 +1,62 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class biblioteca(models.Model):
-#     _name = 'biblioteca.biblioteca'
-#     _description = 'biblioteca.biblioteca'
+class lector(models.Model):
+    _name = 'biblioteca.lector'
+    _description = 'Representa el lector de un libro'
+
+    dni = fields.Char('Dni', required=True, size=7)
+    nombre = fields.Char('Nombre', required=True)
+    prestamos_ids = fields.One2many('biblioteca.prestamo', 'lector_id', string='lector-prestamo')
+
+
+class prestamo(models.Model):
+    _name = 'biblioteca.prestamo'
+    _description = 'Representa el prestamo de un libro'
+
+    identification = fields.Integer(string='Id', max=10) 
+    fechaInicio = fields.Date(string='Fecha de inicio')
+    fechaFin = fields.Date(string='Fecha de fin')
+    lector_id = fields.Many2one('biblioteca.lector', string='prestamo-lector')
+
+
+class libro(models.Model):
+    _name = 'biblioteca.libro'
+    _description = 'Representa un libro'
+
+    isbn = fields.Char('Isbn', required=True, size=17)
+    titulo = fields.Char('Titulo', required=True)
+    numeroDePaginas = fields.Integer(string='numero de paginas') 
+    editorial_ids = fields.One2many('biblioteca.editorial', 'libro_id', string='libro-editorial')
+    autor_ids = fields.Many2many('biblioteca.autor', string='libro-autor')
+   
+
+class editorial(models.Model):
+    _name = 'biblioteca.editorial'
+    _description = 'Representa la editorial de un libro'
+    
+    nombre = fields.Char('Nombre', required=True)
+    libro_id = fields.Many2one('biblioteca.libro', string='editorial-libro')
+
+
+class autor(models.Model):
+    _name = 'biblioteca.autor'
+    _description = 'Representa un autor'
+
+    dni = fields.Char('Dni_autor', required=True, size=7)
+    nombre = fields.Char('Nombre', required=True)
+    nacimiento = fields.Date(string='Fecha de nacimiento del autor')
+    edad = fields.Integer(string='Edad', compute='calculaEdad')
+    libro_ids = fields.Many2many('biblioteca.libro', string='autor-libro')
+
+    @api.depends
+    def calculaEdad(self):
+        for autor in self:
+            #calculamos edad
+            pass
+
 
 #     name = fields.Char()
 #     value = fields.Integer()
--- garaje/models/models.py
+++ garaje/models/models.py
@@ -2,6 +2,11 @@
 
 from odoo import models, fields, api
 
+#hace falta para _get_annios(self)
+from dateutil.relativedelta import *
+from datetime import date
+
+
 
 # class garaje(models.Model):
 #     _name = 'garaje.garaje'
@@ -24,6 +29,13 @@ class aparcamiento(models.Model):
     name = fields.Char('Direccion', required = True)
     plazas = fields.Integer(string='Plazas', required = True)
 
+    #Campos relacionales. En el padre tendríamos la colección de hijos y en hijo tendríamos la entidad padre
+    #En un aparcamiento hay varios coches. Por convenio se utiliza nombreDeLaClase_id si es un solo objeto, o nombreDeLaClase_id si son varios objetos
+    #La relación es oneToMany, porque un aparcamiento puede tener varios coches
+    
+    coche_ids = fields.One2many('garaje.coche', 'aparcamiento_id', string='field_name')
+
+
 class coche(models.Model):
     _name='garaje.coche'
     _description = 'Características de un coche'
@@ -36,11 +48,19 @@ class coche(models.Model):
     #El campo siguiente podría tener algo así como store=True, lo cual significaría que no se recalcularían los años del coche a no ser que se cambie la fecha de construcción
     annios = fields.Integer('Años', compute='_get_annios') #Este campo lo vamos a calcular nosotros, a partir de la fecha de construcción y la actual
     descripcion = fields.Text('Descripcion')
+    averiado = fields.Boolean('Averiado')
+    
+    #Campos relacionales
+    aparcamiento_id = fields.Many2one('garaje.aparcamiento', string='Aparcamiento')
+    mantenimiento_ids = fields.Many2many('garaje.mantenimiento' , string="Mantenimientos")
 
     @api.depends('construido') #Cuando se modifique el parámetro construido en un objeto coche, automáticamente se lanazará esta función
     def _get_annios(self): # el self representa una colección de objetos, en este caso una colección de coches
-        for coche in self:
-            coche.annios = 0 
+        for coche in self: #Cuando ponemos una función definida a un modelo, el self representa una colección de objetos. 
+        #Siempre está pensado para una colección de datos. Aunque yo elija una única entidad, me va a llegar una array con un único elemento
+            hoy = date.today()
+            coche.annios = relativedelta(hoy, coche.construido).years
+
         
 class mantenimiento(models.Model):
     _name = 'garaje.mantenimiento'
@@ -49,3 +69,8 @@ class mantenimiento(models.Model):
     fecha = fields.Date('Fecha', required = True, default = fields.Date.today())
     tipo = fields. Selection (string = 'Tipo ', selection =[('l','lavar'),('r','revisión'),('m','mecánica'), ('p','pintura')], default='l')
     coste = fields.Float('Coste', (8,2), help='Coste total de mantenimeinto') # 5 números naturales más 2 decimales
+
+    #Campos relacionales
+    coche_ids = fields.Many2many('garaje.coche', string='Coches')
+
+
